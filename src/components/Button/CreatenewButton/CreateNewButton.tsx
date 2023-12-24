27ab813368cc7e5b414a83f5d4da60b7388a5136
Akshay Vs <me.akshay.vs@gmail.com>
2023-12-24 09:44:50 -0500

    Feat(SidePanel): ESLINT Fix

--- src/components/Button/CreatenewButton/CreateNewButton.tsx
+++ src/components/Button/CreatenewButton/CreateNewButton.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 type Props = {
     onClick: () => void
     text: string
--- src/fearures/SidePannel/ExpandedSidePanel/ExpandedSidePanel.tsx
+++ src/fearures/SidePannel/ExpandedSidePanel/ExpandedSidePanel.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import AccountInfoLarge from "../../../components/AccountInfo/AccountInfoLarge";
 
 const ExpandedSidePanel = () => {
