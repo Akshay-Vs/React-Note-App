64a2aa2398600e9317ec325f55e0e93ce5876f3b
Akshay Vs <me.akshay.vs@gmail.com>
2023-12-28 17:05:13 +0530

    Fix(CreateNote): Error reading env

--- src/fearures/SidePanel/ExpandedSidePanel/CreateNote/CreateNote.tsx
+++ src/fearures/SidePanel/ExpandedSidePanel/CreateNote/CreateNote.tsx
@@ -1,6 +1,9 @@
 import { useSetQuery } from "../../../../hooks/useSetQuery";
 import postData from "../../../../../utils/postData";
 import ButtonPrimary from "../../../../components/Button/ButtonPrimary";
+import { useUser } from "@clerk/clerk-react";
+
+const url = import.meta.env.VITE_BACKEND_URL;
 
 interface SubmitData {
   text: {
@@ -18,25 +21,23 @@ interface SubmitData {
 
 const CreateNote = () => {
   const SetQuery = useSetQuery();
+  const { user } = useUser();
 
   const handleClick = async () => {
     SetQuery({ view: "new" });
-    const result = await postData<SubmitData>(
-      "http://localhost:5000/createNote",
-      {
-        text: {
-          title: "Untitled",
-          content: "",
-        },
-        info: {
-          timeModified: new Date().toString(),
-          timeCreated: new Date().toString(),
-          fontSize: 16,
-          fontFamily: "Rubik",
-          isPrivate: false,
-        },
-      }
-    );
+    const result = await postData<SubmitData>(`${url}/createNote/${user?.id}`, {
+      text: {
+        title: "Untitled",
+        content: "",
+      },
+      info: {
+        timeModified: new Date().toString(),
+        timeCreated: new Date().toString(),
+        fontSize: 16,
+        fontFamily: "Rubik",
+        isPrivate: false,
+      },
+    });
     SetQuery({ id: result.id, view: "new" });
   };
   return <ButtonPrimary text="Create New" onClick={handleClick} />;
